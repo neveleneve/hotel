8a99bcafeb1e12fa77643fea94b0d584a8b0acfb
neveleneve <pandupandu813@gmail.com>
2024-12-22 19:25:38 +0700

    hdd

--- resources/views/pages/member/hotel/index.blade.php
+++ resources/views/pages/member/hotel/index.blade.php
@@ -1,51 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="my-[58px]">
-        <div class="swiper">
-            <div class="swiper-wrapper aspect-video">
-                <div class="swiper-slide object-fill">
-                    <img src="{{ asset('assets/img/hotel/' . $hotel->id . '.jpg') }}" alt="Shower Image 1">
+    <div class="mt-3 mb-[168px]">
+        <div class="grid grid-cols-1 px-3">
+            <div
+                class="w-full h-full flex flex-col bg-white border border-[--primary-container] rounded-lg shadow-lg lg:mb-3">
+                <div class="relative w-full rounded-t-lg">
+                    <div class="swiper z-10 rounded-t-lg">
+                        <div class="swiper-wrapper aspect-square lg:aspect-video">
+                            <div class="swiper-slide flex justify-center">
+                                <img src="{{ asset('assets/img/hotel/' . $hotel->id . '.jpg') }}" class="object-cover">
+                            </div>
+                            <div class="swiper-slide flex justify-center">
+                                <img src="{{ asset('assets/img/hotel/' . $hotel->id . '.jpg') }}">
+                            </div>
+                        </div>
+                        <div class="text-xs">
+                        </div>
+                        <div class="swiper-button-prev text-[--on-primary]"></div>
+                        <div class="swiper-button-next text-[--on-primary]"></div>
+                    </div>
+                </div>
+                <hr>
+                <div class="flex flex-col justify-between flex-1 p-4">
+                    <div>
+                        <h5 class="text-2xl font-bold text-[--on-primary] min-h-[48px]">
+                            {{ $hotel->name }}
+                        </h5>
+                        <div class="flex items-center mt-2">
+                            <i class="h-4 w-4 fi fi-{{ $hotel->country->flag_code ?? 'xx' }}"></i>
+                            <span class="text-md text-[--on-primary] font-semibold ml-2">
+                                {{ $hotel->country->name ?? 'xx' }}
+                            </span>
+                        </div>
+                        <div class="flex items-center mt-1">
+                            <i class="material-icons text-yellow-500 text-sm">star_half</i>
+                            <span class="ml-1 text-sm font-semibold text-gray-700">3.5</span>
+                        </div>
+                    </div>
+                    <hr class="border-t border-gray-200 my-3">
+                    <div class=" font-bold text-[--on-primary] text-xl">
+                        {{ 'Rp ' . number_format($hotel->price, 0, ',', '.') }}
+                    </div>
                 </div>
             </div>
-            <div class="swiper-button-prev text-[--on-primary]"></div>
-            <div class="swiper-button-next text-[--on-primary]"></div>
         </div>
-        <h1 class="text-2xl font-bold text-[--on-primary] mx-4 mb-3">
-            {{ $hotel->name }}
-        </h1>
-        <span class="font-bold text-[--on-primary]">
-            <i class="mx-4 rounded-full fi fi-{{ $hotel->country->flag_code }} fis"></i>
-            {{ $hotel->country->name }}
-            <hr class="my-1">
-        </span>
-        <h1 class="text-lg font-bold text-[--on-primary] mx-4">
-            Rp. {{ number_format($hotel->price, 0, ',', '.') }}
-        </h1>
+        <div class="grid grid-cols-1 px-3">
+            <div
+                class="w-full h-full flex flex-col bg-white border border-[--primary-container] rounded-lg shadow-lg lg:mb-3 p-3 mt-3">
+                <h3 class="font-bold text-xl">Jadwal</h3>
+                <hr class="my-2">
+                <div class="grid grid-cols-2">
+                    <div class="block flex-1">
+                        <label for="start_date" class="font-bold text-md">Dari</label>
+                        <input type="date" name="start_date" id="start_date"
+                            class="mt-2 w-full rounded-l-full py-2 px-3 border border-[--on-primary] focus:ring-primary focus:border-primary">
+                    </div>
+                    <div class="block flex-1">
+                        <label for="start_date" class="font-bold text-md">Sampai</label>
+                        <input type="date" name="start_date" id="start_date"
+                            class="mt-2 w-full rounded-r-full py-2 px-3 border border-[--on-primary] focus:ring-primary focus:border-primary">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
-@push('nav')
-    <header class="fixed top-0 left-0 w-full bg-[--on-primary] text-white p-2 flex justify-between items-center z-20 ">
-        <a href="{{ route('landing') }}"
-            class="flex items-center p-2 bg-[--primary] rounded-full border border-transparent hover:bg-transparent hover:border-[--primary]">
-            <i class="material-icons font-bold">chevron_left</i>
-        </a>
-
-    </header>
-@endpush
-
 @push('tab')
-    <nav class="fixed bottom-0 left-0 w-full bg-[--on-primary] text-white  justify-around py-4 rounded-top">
+    <nav class="fixed bottom-0 left-0 w-full bg-[--on-primary] text-white  justify-around py-4 rounded-top z-20">
         <div class="px-4">
-            <div class="grid grid-cols-4 gap-4">
+            <div class="grid grid-cols-4 gap-4 mb-3">
                 <a href="#"
                     class="bg-[--primary] text-[--on-primary] px-10 py-3 w-full rounded-full font-bold hover:bg-[--on-primary] hover:text-[--primary] hover:border hover:border-[--primary] border border-transparent col-span-3 text-center">
                     Pesan
                 </a>
                 <a href="#"
                     class="text-[--primary] border border-[--primary] px-3 py-2 w-full rounded-full font-bold hover:bg-[--primary] hover:border-transparent hover:text-[--on-primary] text-center">
-                    <i class="material-icons  ">shopping_bag</i>
+                    <i class="material-icons ">shopping_bag</i>
+                </a>
+            </div>
+            <div class="grid grid-cols-1 gap-4">
+                <a href="{{ route('landing') }}"
+                    class="bg-[--error] text-[--on-error] px-10 py-3 w-full rounded-full font-bold hover:bg-[--on-error] hover:text-[--error] hover:border hover:border-transparent border border-transparent text-center">
+                    Kembali
                 </a>
             </div>
         </div>
@@ -59,12 +97,21 @@
         .swiper-button-next {
             color: var(--primary);
         }
+
+        .swiper-button-prev::after,
+        .swiper-button-next::after {
+            font-size: 1.25rem;
+            /* Atur ukuran font ikon */
+        }
     </style>
+    {{-- <style>
+        </style> --}}
 @endpush
 @push('customjs')
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
         var swiper = new Swiper('.swiper', {
+            loop: true,
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
