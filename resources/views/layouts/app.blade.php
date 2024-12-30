92057a91db8aa1bfdd61ba0b62cb24124685ab2a
neveleneve <pandupandu813@gmail.com>
2024-12-30 20:07:49 +0700

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Belanja.com</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
