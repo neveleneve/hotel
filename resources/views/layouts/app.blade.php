86320a9e9d3554459ee3a6afb2dd1a1e5cb8ac8b
neveleneve <pandupandu813@gmail.com>
2024-12-30 21:15:34 +0700

    udpate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/icon.jpeg') }}" type="image/x-icon">
     <title>{{ env('APP_NAME') }}</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
