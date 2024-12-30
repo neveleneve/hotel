e479343a370ad7c8c3b496efbcd5713290f18db9
neveleneve <pandupandu813@gmail.com>
2024-12-30 20:16:26 +0700

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,21 @@
     @yield('content')
     @stack('footer')
     @stack('tab')
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/6772787baf5bfec1dbe48728/1igbid98q';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @stack('customjs')
     @livewireScripts
