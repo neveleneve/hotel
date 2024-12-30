0950eb650cb0e57ef29e0eec82721141fe3ac7e0
neveleneve <pandupandu813@gmail.com>
2024-12-30 22:22:53 +0700

    logo and member message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="{{ asset('assets/img/icon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
     <title>{{ env('APP_NAME') }}</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
@@ -33,7 +33,6 @@
             s0.parentNode.insertBefore(s1, s0);
         })();
     </script>
-    <!--End of Tawk.to Script-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @stack('customjs')
     @livewireScripts
