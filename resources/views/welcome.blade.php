d8cea81e2149adc022a3e9a653bacd68cc1988fd
neveleneve <pandupandu813@gmail.com>
2024-12-30 22:36:07 +0700

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
     <header class="fixed top-0 left-0 w-full bg-[--on-primary] text-white p-4 flex justify-between items-center z-20">
         <div class="flex items-center">
             <a href="{{ route('profile.index') }}">
-                <img class="inline-block h-8 w-8 rounded-lg ring-1 ring-white" src="{{ asset('assets/img/logo.jpg') }}">
+                <img class="inline-block h-8 w-8 rounded-lg ring-1 ring-white" src="{{ asset('assets/img/logo.png') }}">
             </a>
             <a href="{{ route('landing') }}" class="ml-4 font-bold">{{ env('APP_NAME') }}</a>
         </div>
