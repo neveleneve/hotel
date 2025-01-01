dc09622d2b41950f37cce729160abd4ce16e1cfd
neveleneve <pandupandu813@gmail.com>
2025-01-02 00:07:10 +0700

    up

--- database/migrations/2025_01_01_235401_modify_member_messages_table.php
+++ database/migrations/2025_01_01_235401_modify_member_messages_table.php
@@ -2,21 +2,27 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void {
         Schema::table('member_messages', function (Blueprint $table) {
-            $table->integer('discount')->default(0);
-            $table->boolean('discount_status')->default(false);
-            $table->boolean('active')->default(true)->change();
+            if (!Schema::hasColumn('member_messages', 'discount')) {
+                $table->integer('discount')->default(0);
+            }
+            if (!Schema::hasColumn('member_messages', 'discount_status')) {
+                $table->boolean('discount_status')->default(false);
+            }
+
+            // Ubah default value kolom active
+            DB::statement('ALTER TABLE member_messages ALTER COLUMN active SET DEFAULT true');
         });
     }
 
     public function down(): void {
         Schema::table('member_messages', function (Blueprint $table) {
-            $table->dropColumn(['discount', 'discount_status']);
-            $table->boolean('active')->default(false)->change();
+            DB::statement('ALTER TABLE member_messages ALTER COLUMN active SET DEFAULT false');
         });
     }
 };
